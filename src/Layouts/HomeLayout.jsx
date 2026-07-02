6467bcef9d37ef99177910934df2dbdf7f06e1c3
rafiulhasan1 <rafiulhasan001@gmail.com>
2026-07-02 22:53:06 +0600

    added navber part

--- src/Layouts/HomeLayout.jsx
+++ src/Layouts/HomeLayout.jsx
@@ -1,16 +1,19 @@
 import Home from "../Components/Home";
 import LatestNews from "../Components/LatestNews";
+import Navber from "../Components/Navber";
 
 const HomeLayout = () => {
     return (
-        <div>
+        <div className="font-poppins">
             <header>
                 <Home></Home>
                 <section className="w-9/12 mx-auto">
                     <LatestNews/>
                 </section>
             </header>
-            <nav></nav>
+            <nav className="w-9/12 mx-auto mt-4">
+                <Navber/>
+            </nav>
             <main></main>
         </div>
     );
