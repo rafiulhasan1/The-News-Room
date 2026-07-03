afbd8a2b65f1581c95b10dbb562d2f40dbb24a8a
rafiulhasan1 <rafiulhasan001@gmail.com>
2026-07-03 20:32:09 +0600

    set marque part auto data load

--- src/Components/LatestNews.jsx
+++ src/Components/LatestNews.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import * as MarqueeModule from "react-fast-marquee";
 import { Link } from "react-router-dom";
 
@@ -5,19 +6,20 @@ const Marquee = MarqueeModule.default.default ?? MarqueeModule.default;
 
 const LatestNews = () => {
 
+    const [loadTitle, setLoadTitle] = useState([]);
+    useEffect(() => {
+        fetch("https://openapi.programming-hero.com/api/news/category/08")
+            .then(res => res.json())
+            .then(data => setLoadTitle(data.data))
+    }, [])
+
     return (
         <div className="flex items-center gap-2 bg-gray-100 p-3">
             <p className="bg-[#D72050] text-white py-1 px-2">Latest</p>
-            <Marquee >
-                <Link to="/news" className="mx-6">
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui tenetur saepe ipsum laborum nulla quos dicta maiores itaque a ipsa.
-                </Link>
-                <Link to="/news" className="mx-6">
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui tenetur saepe ipsum laborum nulla quos dicta maiores itaque a ipsa.
-                </Link>
-                <Link to="/news" className="mx-6">
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui tenetur saepe ipsum laborum nulla quos dicta maiores itaque a ipsa.
-                </Link>
+            <Marquee speed={100} >
+                {
+                    loadTitle.map((news) => <p className="font-semibold mr-8 text-gray-600" key={news._id}>{news.title}</p>)
+                }
             </Marquee>
         </div>
     );
